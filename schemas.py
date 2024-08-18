69749bab8a4812af4e7d54b6c2ba57146f82ae27
IgnacioMansillaGomez <ignaciomansilladeveloper@gmail.com>
2024-08-17 22:57:51 -0300

    added database , schemas , models , utils

---
+++ schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class TranslationRequestSchema(BaseModel):
+    text: str
+    languages: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "text": "Hello, world!",
+                "languages": "english, german, russian , spanish"
+            }
+        }
